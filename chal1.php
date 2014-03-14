171d82efd292f96309f4553140b18f75e03f4884
seeafish <siagholami@gmail.com>
2014-03-14 10:37:13 +0000

    Scripts builds servers now. Polling and checking to come

--- chal1.php
+++ chal1.php
@@ -30,15 +30,36 @@ if (empty($arguments)) {
 // be able to customise the image/flavour
 $cs = $cloud->computeService('cloudServersOpenStack', 'LON');
 
-echo "Creating $num Ubuntu 13.10 512MB servers named\n";
+// get images and flavours
+$images = $cs->imageList();
+$flavours = $cs->flavorList();
+
+// as much I hate this, loop through and find the objects required
+while ($image = $images->next()) {
+	if (strpos($image->id, 'bbb6b40f-fc70-4e7e-ab63-42b4a0d47997') !== false) {
+        $build_image = $image;
+        break;
+    }
+}
+
+$flavours = $cs->flavorList();
+while ($flavour = $flavours->next()) {
+	if (strpos($flavour->name, '512MB Standard Instance') !== false) {
+		$build_flavour = $flavour;
+		break;
+	}
+}
+
+// populate array of servers to build
+echo "Creating $num $build_image->name $build_flavour->name servers named\n";
 for ($i = 1; $i <= $num; $i++) {
     echo "$hostname$i\n";
 
     // populate an array to create servers
     $srv_details[$i] = array(
         'name' => $hostname.$i,
-        'image' => '5a8759da-dca8-40ce-86fa-fa029a58a999',
-        'flavor' => 2,
+        'image' => $build_image,
+        'flavor' => $build_flavour,
         'networks' => array(
             $cs->network(Network::RAX_PUBLIC),
             $cs->network(Network::RAX_PRIVATE)
@@ -48,5 +69,14 @@ for ($i = 1; $i <= $num; $i++) {
 
 // instantiate the $srv object for creation and polling
 $srv = $cs->server();
-$response = $srv->create($srv_details[2]);
 
+foreach ($srv_details as $server) {
+    try {
+        $srv->create($server);
+    } catch (\Guzzle\Http\Exception\BadResponseException $e) {
+        $responseBody = (string) $e->getResponse()->getBody();
+        $statusCode = $e->getResponse()->getStatusCode();
+        $headers = $e->getResponse()->getHeaderLines();
+        echo sprintf("Status: %s\nBody: %s\nHeaders: %s", $statusCode, $responseBody, implode(', ', $headers));
+    }
+}
