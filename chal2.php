00c480af672b302626dda3e5c1d5d6cfba9af084
seeafish <siagholami@gmail.com>
2014-03-22 10:00:36 +0000

    Image feature now working

--- chal2.php
+++ chal2.php
@@ -1,8 +1,4 @@
 <?php
-// Outline:
-// 1. Take UUID as argument and snapshot the server
-// 2. If not UUID is specified, print a list of available servers
-// 3. Error handling the imageList loop if no image is found? Maybe...
 
 include 'auth.php';
 
@@ -13,7 +9,7 @@ $cs = $cloud->computeService('cloudServersOpenStack', 'LON');
 // get arg list
 $arguments = getopt("u:");
 if (empty($arguments)) {
-    echo 'Usage: chal2.php -u UUID:\n';
+    echo "Usage: chal2.php -u UUID:\n";
     $serverlist = $cs->serverList();
     echo "Here is a list of servers to choose from:\n";
     while ($server = $serverlist->next()) {
@@ -35,19 +31,38 @@ date_default_timezone_set('UTC');
 // get server and kick off image
 $srv = $cs->server($uuid);
 $clone_name = $srv->name . '-' . date('Y-m-d-H:i');
-//$srv->createImage($clone_name);
+echo "Creating image and server $clone_name...\n";
+#$srv->createImage($clone_name);
 
-// find image and store object
-$imagelist = $cs->imageList();
-while ($image = $imagelist->next()) {
-    //if (strpos($image->name, $clone_name) !== false) {
-    if (strpos($image->name, 'web1-2014-03-20-15:26') !== false) {
-        $img = $image;
+// find a store image object
+#$img = getImage($cs, $clone_name);
+$img = getImage($cs, 'web1-2014-03-22-09:48');
+
+// wait for image to save
+echo "Waiting for image to save...\n";
+while (true) {
+    #sleep(30);
+    sleep(2);
+    if ($img->status != 'SAVING') {
+        echo "Image saved!\n";
+        break;
     }
+    echo "Image status: $img->status\n";
+    #$img = getImage($cs, $clone_name);
+    $img = getImage($cs, 'web1-2014-03-22-09:48');
 }
 
-// wait for image to finish
-while ($img->status == 'SAVING') {
-    echo "saving...\n";
-    sleep(10);
+// create server
+$srv = $cs->server();
+
+
+
+function getImage($cs, $img_name) {
+    $imagelist = $cs->imageList();
+    while ($image = $imagelist->next()) {
+        if (strpos($image->name, $img_name) !== false) {
+            $img = $image;
+        }
+    }
+    return $img;
 }
