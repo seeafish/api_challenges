0188219c650bd123a4adb7430e02ddd0d41053f7
seeafish <siagholami@gmail.com>
2014-03-30 18:52:00 +0100

    Upload functionality working

--- chal3.php
+++ chal3.php
@@ -2,6 +2,8 @@
 
 include 'auth.php';
 
+use OpenCloud\ObjectStore\Resource\DataObject;
+
 $cf = $cloud->objectStoreService('cloudFiles', 'LON');
 
 $arguments = getopt("c:d:l");
@@ -15,35 +17,39 @@ if (empty($arguments)) {
 } else {
     $cont_name = $arguments["c"];
     $dir_name = $arguments["d"];
-    if (!is_dir($dir_name)) {
-        echo "Invalid directory!\n";
-        exit(1);
-    }
-    
+        
     if (!isset($cont_name) && !isset($dir_name)) {
         if (isset($arguments["l"])) {
             $containerlist = $cf->listContainers();
             while ($container = $containerlist->next()) {
                 echo "$container->name\n";
             }
+            exit(1);
         }
     }
 }
 
-
-// start everything off
-echo "Uploading $dir_name into $cont_name...\n";
-
 // store files in an array
 $i = 0;
-$dh = opendir($dir_name);
-while (($file = readdir($dh)) !== false) {
-    if ($file == '.' || $file == '..') {
-        continue;
+if (!is_dir($dir_name)) {
+    echo "Invalid directory!\n";
+    exit(1);
+} elseif ($dh = opendir($dir_name)) {
+    // sanitise last slash
+    if (substr($dir_name, -1) == "/") {
+        $dir_name = substr($dir_name, 0, -1);
     }
-    $objects[$i] = $file;
-    $i++;
+    while (($file = readdir($dh)) !== false) {
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
+        $objects[$i]["name"] = $file;
+        $objects[$i]["path"] = $dir_name . '/' . $file;
+        $i++;
+    }
+    closedir($dh);
 }
-closedir($dh);
 
-var_dump($objects);
+$container = $cf->getContainer($cont_name);
+echo "Uploading files from $dir_name into container $cont_name...\n";
+$container->uploadObjects($objects);
