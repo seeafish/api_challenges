fac71998d96d8f3bd647819a7c7b03265edd9ba3
seeafish <siagholami@gmail.com>
2014-04-03 11:43:58 +0100

    Challenge 5 complete

--- chal5.php
+++ chal5.php
@@ -18,8 +18,15 @@ if (empty($arguments["i"]) || empty($arguments["d"]) || empty($arguments["s"])) 
     exit(1);
 } else {
     $instance_name = $arguments["i"];
-    $disk_size = $arguments["d"];
     
+    // sanitise disk size by stripping out non numbers
+    $disk_size = preg_replace('/[^0-9*]/', '', $arguments["d"]);
+    if (!($disk_size >= 1 && $disk_size <= 150)) {
+        echo "Incorrect disk size, please enter a number between 1 - 150 (GB)\n";
+        exit(1);
+    }
+        
+    // convert flavour
     switch (strtolower($arguments["s"])) {
         case "512mb":
             $flavour = 1;
@@ -46,13 +53,64 @@ if (empty($arguments["i"]) || empty($arguments["d"]) || empty($arguments["s"])) 
 
     if (isset($arguments["n"])) {
         $db_name = $arguments["n"];
+    } else {
+        $db_name = $arguments["i"] . 'db1';
     }
     if (isset($arguments["u"])) {
         $db_user = $arguments["u"];
+    } else {
+        $db_user = $db_name . 'user1';
     }
     if (isset($arguments["p"])) {
         $db_pass = $arguments["p"];
+    } else {
+        $db_pass = genPassword();
     }
 }
-echo "$instance_name\n$disk_size\n$flavour\n$db_name\n$db_user\n$db_pass\n";
-exit;
+
+// create instance
+echo "Creating instance $instance_name\n";
+$db = $cloud->databaseService('cloudDatabases', 'LON');
+$db_instance = $db->instance();
+
+$db_instance->name = $instance_name;
+$db_instance->volume = new stdClass();
+$db_instance->volume->size = $disk_size;
+$db_instance->flavor = $db->flavor($flavour);
+$db_instance->create();
+
+echo "Waiting for instance to finish building...\n";
+$db_instance->waitFor(State::ACTIVE, null, function ($db_instance) {
+    echo "Status: $db_instance->status\n";
+});
+
+// create db and user
+echo "Creating new database. Connections details:\n" .
+"\nInstance hostname: $db_instance->hostname\n" .
+"DB name: $db_name\n" .
+"DB user: $db_user\n" .
+"DB password: $db_pass\n\n";
+
+$new_db = $db_instance->database();
+$new_db->name = $db_name;
+$new_db->create();
+
+// could do something with $db_instance->databaseList() here
+// to check if the new db is there before a user is created
+// but the existing functionality seemingly works fine
+
+$new_user = $db_instance->user();
+$new_user->name = $db_user;
+$new_user->password = $db_pass;
+$new_user->databases = array($db_name);
+$new_user->create();
+
+echo "Done!\n";
+
+
+// why reinvent the wheel...
+function genPassword($length = 12) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$&*-=+?";
+    $password = substr( str_shuffle( $chars ), 0, $length );
+    return $password;
+}
